64e50714af09af9d90b6a952659fe0970931ed5a
Sébastien <shopin@laposte.net>
2025-10-22 10:51:53 +0200

    Ajout header

---
+++ projetorphelinat/header.php
@@ -0,0 +1,26 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Header</title>
+</head>
+<body>
+    <header>
+
+        <nav>
+            <ul>
+                <li>Accueil</li>
+                <div class = "centre">
+                <li>Adopter</li>
+                <li>Combattre</li>
+                <li>Recycler</li>
+                </div>
+                <li class = "deco">Déconnexion</li>
+            </ul>
+        </nav>
+    
+
+    </header>
+</body>
+</html>
