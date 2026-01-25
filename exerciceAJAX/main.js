b6b2c390689e7f7c04e8cf4265ce9b7472217e6c
Sébastien <shopin@laposte.net>
2026-01-25 21:09:53 +0100

    ajout ex ajax

---
+++ exerciceAJAX/main.js
@@ -0,0 +1,13 @@
+// let btn = document.getElementById("btn");
+// let list = document.getElementById("liste");
+
+// btn.addEventListener("click", () => {
+//   fetch("https://jsonplaceholder.typicode.com/users", {
+//     headers: { "x-api-key": "DEMO-API-KEY" }
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//     })
+//     .catch(error => console.log("error", error));
+// });
